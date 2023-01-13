3eae474e409f4770f56a11556a2fb5877451409c
Mohamed Halfawy <mohamed.elhalfawyy@gmail.com>
2023-01-13 05:34:18 +0200

    BUGFIX/Fix Packages bug

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const config = require("config");
 const app = express();
 
 require('./Startup/logging')();
-require("./startup/cors")(app);
+// require("./startup/cors")(app);
 require('./Startup/routes')(app);
 require('./Startup/db')();
 require('./Startup/config')();
 require('./Startup/validation')();
+require('./Startup/prod')(app);
 
 const port = process.env.PORT || config.get("port");
 const server = app.listen(port, () => console.log(`I am listening on port ${port}...`));
