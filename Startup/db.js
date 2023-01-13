5bf7a31232b49e570523a5a9508cb0034f9e73d6
Mohamed Halfawy <mohamed.elhalfawyy@gmail.com>
2023-01-13 02:30:58 +0200

    BUGFIX/Fix_Database_Connection

--- Startup/db.js
+++ Startup/db.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
+const config = require('config');
 
 module.exports = async function () {
+    const db = config.get('db');
+
     mongoose.set("strictQuery", false);
-    await mongoose.connect('mongodb://127.0.0.1:27017/zoe', {useNewUrlParser: true, useUnifiedTopology: true})
+    await mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
         .then(() => console.log('Connected to MongoDB...'));
 }
