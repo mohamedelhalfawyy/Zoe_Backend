c3206f70de25de3758e7083d516d4761cfe41e93
Mohamed Halfawy <mohamed.elhalfawyy@gmail.com>
2023-01-14 20:58:54 +0200

    Test/Testing function

--- MiddleWare/error.js
+++ MiddleWare/error.js
@@ -3,5 +3,5 @@ const winston = require('winston');
 module.exports = function (err, req, res) {
     winston.error(err.message, err);
 
-    // res.status(500).send('Something went wrong');
+    res.status(500).send('Something went wrong');
 }
